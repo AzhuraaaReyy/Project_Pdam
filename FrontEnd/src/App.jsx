b24b3d7fee96e201ed02acbbc440b5d63c545854
AzhuraaaReyy <azhurareyy@gmail.com>
2025-05-19 00:18:12 +0700

    completeDashboard2

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignInPage from "./pages/SignIn";
 import SignUpPage from "./pages/SignUp";
-import DashboardPage from "./pages/Dashboardd";
+import DashboardPage from "./pages/dashboard";
 import MenuPelanggan from "./pages/MenuPelanggan";
 const App = () => {
   const myrouter = createBrowserRouter([
