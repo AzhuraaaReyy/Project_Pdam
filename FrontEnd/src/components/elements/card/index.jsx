a71cb57bbfdd13a11109ddecdc25c56a421e3415
AzhuraaaReyy <azhurareyy@gmail.com>
2025-05-21 23:34:56 +0700

    fix1

--- FrontEnd/src/components/elements/card/index.jsx
+++ FrontEnd/src/components/elements/card/index.jsx
@@ -1,20 +1,17 @@
 const Card = (props) => {
-  const { title = false, desc, variant, children } = props;
+  const { title = false, desc, variant } = props;
 
   return (
-    <div
-      className={`flex flex-col flex-1 mb-6 ${variant}`}
-      // Tambahkan ini
-    >
+    <div className={`flex flex-col flex-1 mb-6 ${variant}`}>
       {title &&
-        (title.length === 1 ? (
+        (title.length == 1 ? (
           <div className="hidden md:block md:text-lg md:text-gray-02 md:mb-4">
             {title}
           </div>
         ) : (
-          <div className="text-lg text-black mb-4 text-white">{title}</div>
+          <div className="text-lg text-gray-02 mb-4">{title}</div>
         ))}
-      <div className="bg-blue-500 rounded-lg px-6 py-5 shadow-xl flex-1">
+      <div className="bg-white rounded-lg px-6 py-5 shadow-xl flex-1">
         {desc}
       </div>
     </div>
